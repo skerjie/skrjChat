78d86ce7af0e30fcf9a2774ec9686fcdbdd6a63c
skerjie <skerjie@live.com>
2016-08-10 17:19:53 +0300

    Add code from lecture to setting tableView cell

--- ViewController.swift
+++ ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseAuth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UITextFieldDelegate {
     
+    var messages: [FIRDataSnapshot]! = [FIRDataSnapshot]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var textField: UITextField!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         
@@ -20,10 +25,38 @@ class ViewController: UIViewController {
             self.navigationController?.present(vc!, animated: true, completion: nil)
         }
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        
+        let messageSnap: FIRDataSnapshot! = self.messages[indexPath.row]
+        let message = messageSnap.value as! Dictionary<String,String>
+        let text = message["text"] as String!
+        cell.textLabel?.text = text
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.textField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("ended editing")
+        self.view.endEditing(true)
+        return true
     }
 
     override func didReceiveMemoryWarning() {
